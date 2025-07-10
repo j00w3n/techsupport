9206622921f559be078688989dfb4629c87d2721
Ezzuan Azizon <ezzuana@gmail.com>
2025-07-10 17:29:04 +0800

    add sql file

--- jobsheet.sql
+++ jobsheet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2025 at 10:57 AM
+-- Generation Time: Jul 10, 2025 at 11:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,21 +31,19 @@ CREATE TABLE `jobsheet` (
   `id` int(11) NOT NULL,
   `date` date DEFAULT NULL,
   `time` time DEFAULT NULL,
-  `hotelname` varchar(255) DEFAULT NULL,
   `complaint` text DEFAULT NULL,
   `fault` text DEFAULT NULL,
   `repair` text DEFAULT NULL,
-  `partreplaced` text DEFAULT NULL
+  `hotel_id` int(11) DEFAULT NULL,
+  `person_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `jobsheet`
 --
 
-INSERT INTO `jobsheet` (`id`, `date`, `time`, `hotelname`, `complaint`, `fault`, `repair`, `partreplaced`) VALUES
-(1, '2025-07-08', '14:19:00', 'concorde shah alam', 'news not running', 'modulator hang', 'reboot the modulator', '-'),
-(2, '2025-07-08', '14:48:00', '-', '-', '-', '-', '-'),
-(3, '2025-07-08', '14:48:00', '-', '-', '-', '-', '-');
+INSERT INTO `jobsheet` (`id`, `date`, `time`, `complaint`, `fault`, `repair`, `hotel_id`, `person_id`) VALUES
+(1, '2025-07-08', '14:19:00', 'news not running', 'modulator hang', 'reboot the modulator', 1, NULL);
 
 --
 -- Indexes for dumped tables
@@ -55,7 +53,9 @@ INSERT INTO `jobsheet` (`id`, `date`, `time`, `hotelname`, `complaint`, `fault`,
 -- Indexes for table `jobsheet`
 --
 ALTER TABLE `jobsheet`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_jobsheet_hotel` (`hotel_id`),
+  ADD KEY `fk_jobsheet_person` (`person_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -66,6 +66,17 @@ ALTER TABLE `jobsheet`
 --
 ALTER TABLE `jobsheet`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jobsheet`
+--
+ALTER TABLE `jobsheet`
+  ADD CONSTRAINT `fk_jobsheet_hotel` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_jobsheet_person` FOREIGN KEY (`person_id`) REFERENCES `hotel_person` (`picid`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
